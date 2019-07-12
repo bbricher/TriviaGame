6554b150061c80d84dcbf5c1d2dd2ca612c7db69
Briana Bricher <bbricher@udallas.edu>
2019-07-12 07:58:52 -0500

    created initial files and added some psuedo code

---
+++ javascript/app.js
@@ -0,0 +1,22 @@
+
+// create variables
+// create each question as an object
+//          add answers, losing gif/img, and winning gif/img
+// build game start function
+// build game reset function
+// create interval function for the questions
+// create if statements for winning and losing
+// BONUS: create a spoiler alert action before the last 3 questions
+//             function end game button
+
+
+
+
+
+
+
+
+
+
+
+
